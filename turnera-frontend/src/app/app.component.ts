ee1512835b9ab1ec0f95e500c43eb9d731d1b792
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2025-01-20 17:49:21 -0300

    init sidenav component

--- turnera-frontend/src/app/app.component.ts
+++ turnera-frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './layouts/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ turnera-frontend/src/app/layouts/sidenav/sidenav.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-sidenav',
+  imports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent {
+
+}
