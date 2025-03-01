71b9238a507ac2ed654c35da7c32c7d7379004a3
Nahuel Monje <102997844+monjenahuel@users.noreply.github.com>
2025-03-01 03:46:19 -0300

    Mejora en los estilos de los eventos, eliminacion de bordes

--- turnera-frontend/src/app/components/calendario/calendario.component.ts
+++ turnera-frontend/src/app/components/calendario/calendario.component.ts
@@ -13,18 +13,49 @@ class EventInputCustom implements EventInput {
   title?: string | undefined;
   start?: DateInput | undefined;
   end?: DateInput | undefined;
+  classNames?: string[] | undefined;
+  backgroundColor?: string | undefined;
+  textColor?: string | undefined;
+  allDay?: boolean | undefined;
   profesional?: string;
+  cliente?: string;
+  display?: string | undefined;
+  className?: (string | string[]) | undefined;
+}
+
+function getBackgroundColorByProfesional(profesional: string) {
+  let output = ''
+  switch (profesional) {
+    case 'Juan Perez':
+      output = 'red';
+      break;
+    case 'Maria Rodriguez':
+      output = 'green';
+      break;
+    case 'Lucas Mandela':
+      output = 'blue';
+      break;
+    default:
+      output = 'gray';
+  }
+
+  return output
 }
 
 let eventGuid = 0;
 const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+const TOMORROW_STR = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of tomorrow
 
 export let INITIAL_EVENTS: EventInputCustom[] = [
   {
     id: createEventId(),
     title: 'Instalacion aire acondicionado',
-    start: TODAY_STR,
+    start: TODAY_STR + 'T16:00:00',
+    end: TODAY_STR + 'T20:00:00',
     profesional: 'Juan Perez',
+    cliente: 'Lucia Lucero',
+    backgroundColor: getBackgroundColorByProfesional("Juan Perez"),
+    display: 'block',
   },
   {
     id: createEventId(),
@@ -32,6 +63,10 @@ export let INITIAL_EVENTS: EventInputCustom[] = [
     start: TODAY_STR + 'T09:00:00',
     end: TODAY_STR + 'T11:00:00',
     profesional: 'Maria Rodriguez',
+    cliente: 'Nicolas Arcamone',
+    backgroundColor: getBackgroundColorByProfesional("Maria Rodriguez"),
+    display: 'block',
+
   },
   {
     id: createEventId(),
@@ -39,6 +74,37 @@ export let INITIAL_EVENTS: EventInputCustom[] = [
     start: TODAY_STR + 'T12:00:00',
     end: TODAY_STR + 'T15:00:00',
     profesional: 'Juan Perez',
+    cliente: 'Evelyn Gorria',
+    backgroundColor: getBackgroundColorByProfesional("Juan Perez"),
+    display: 'block',
+  },
+  {
+    id: createEventId(),
+    title: 'Prueba de sonido',
+    start: TODAY_STR + 'T11:00:00',
+    end: TODAY_STR + 'T12:00:00',
+    profesional: 'Lucas Mandela',
+    cliente: 'Pablos Lopes',
+    backgroundColor: getBackgroundColorByProfesional("Lucas Mandela"),
+    display: 'block',
+  },
+  {
+    id: createEventId(),
+    title: 'Prueba de imagen',
+    start: TODAY_STR + 'T11:00:00',
+    end: TODAY_STR + 'T12:00:00',
+    profesional: 'Maria Rodriguez',
+    cliente: 'Dina Choque',
+    display: 'block',
+  },
+  {
+    id: createEventId(),
+    title: 'Evento dia siguiente',
+    start: TOMORROW_STR + 'T11:00:00',
+    end: TOMORROW_STR + 'T12:00:00',
+    profesional: 'Maria Rodriguez',
+    cliente: 'Dina Choque',
+    display: 'block',
   }
 ];
 
@@ -69,6 +135,8 @@ export class CalendarioComponent {
 
   calendarVisible = signal(true);
   calendarOptions = computed<CalendarOptions>(() => ({
+    themeSystem: 'bootstrap5',
+    //eventBorderColor: 'black', // Tambien altera el "punto" de la agenda
     locale: esLocale,
     plugins: [
       interactionPlugin,
@@ -88,10 +156,10 @@ export class CalendarioComponent {
     selectable: true,
     selectMirror: true,
     dayMaxEvents: true,
+    displayEventTime: false, //No muestra la hora del evento
     select: this.handleDateSelect.bind(this),
     eventClick: this.handleEventClick.bind(this),
     eventsSet: this.handleEvents.bind(this),
-    themeSystem: 'standard',
   }));
 
   constructor(private changeDetector: ChangeDetectorRef) {}
